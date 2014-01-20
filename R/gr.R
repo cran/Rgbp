9b95c574392a13d12afbb432688f7887accb956a
Joseph Kelly <kelly2@fas.harvard.edu>
2014-01-20 21:33:35 +0000

    version 1.0.4

--- R/gr.R
+++ R/gr.R
@@ -26,20 +26,19 @@ gr<-function(y,se,X,mu,Alpha=0.95,intercept=T,eps=0.0001, normal.CI = FALSE){
   if(muknown){
     r<-0
     est <- optim(mean(log(V)),function(x){gr.ll.muknown(exp(x),y,V,mu,type)},control=list(fnscale=-1),method="BFGS",hessian=T)
-    est.var <- solve(-1*as.matrix(est$hessian))
     Ahat <- exp(est$par)
     Betahat<-NA
     Betahatvar<-NA
   }else{
     r <- dim(X)[2]
-    est <- optim(mean(log(V)), function(x){gr.ll.muunknown(exp(x[1]),y,V,X,type)},control=list(fnscale=-1),method="BFGS",gr = function(x){derval(x,y,V,X)}, hessian=T)
-    est.var <- -1/est$hessian
+    est <- optim(mean(log(V)), function(x){gr.ll.muunknown(exp(x[1]),y,V,X,type)},control=list(fnscale=-1),method="BFGS", hessian=T, gr = function(x){derval(x,y,V,X)})
     Ahat <- exp(est$par[1])
   }
 
   ##calculate estimates
-  ninfo <- -1*as.matrix(est$hessian)[1,1]
-  Avar<-est.var[1,1]*Ahat^2
+  est.var <- -1/est$hessian
+  ninfo <- -1*est$hessian
+  Avar<-est.var*Ahat^2
   Bhat<-V/(V+Ahat)
   a0<-ninfo/Bhat
   a1<-ninfo/(1-Bhat)
@@ -55,16 +54,18 @@ gr<-function(y,se,X,mu,Alpha=0.95,intercept=T,eps=0.0001, normal.CI = FALSE){
   }else{
     DVAhat<-diag(V+Ahat)
     DVAhati <- diag(1/(V+Ahat))
-    Betahat <- solve(t(X)%*%DVAhati%*%X)%*%t(X)%*%DVAhati%*%y
-    Betahatvar <- solve(t(X)%*%DVAhati%*%X)
+    Betahatvar <- chol2inv(chol(t(X)%*%DVAhati%*%X))
+    BetahatvarmX <- Betahatvar%*%t(X)
+    Betahat <- BetahatvarmX%*%DVAhati%*%y
     BetahatSE <- sqrt(diag(Betahatvar))
     mu<-X%*%Betahat
     ##calculate posterior variance
-    E <- eigen(DVAhat)
-    W <- E$values
-    Q <- E$vectors
-    Z <- Q%*%diag(1/sqrt(W))%*%t(Q)
-    Phat<-Z%*%X%*%solve(t(X)%*%solve(DVAhat)%*%X)%*%t(X)%*%Z
+    #E <- eigen(DVAhat)
+    #W <- E$values
+    #Q <- E$vectors
+    #Z <- Q%*%diag(1/sqrt(W))%*%t(Q)
+    Z <- sqrt(DVAhati)
+    Phat<-Z%*%X%*%BetahatvarmX%*%Z
     p<-diag(Phat)
     shat<-sqrt((1-(1-p)*Bhat)*V+v*(y-mu)^2)
   }
@@ -75,9 +76,9 @@ gr<-function(y,se,X,mu,Alpha=0.95,intercept=T,eps=0.0001, normal.CI = FALSE){
   skew<-((mu-y)^3*mu3-3*V*(mu-y)*v)/shat^3
   sqrtV<-sqrt(V)
   hmB <- length(Bhat)/sum(1/Bhat)
-  m4B <- (6*(a0^3 - a0^2*(2*a1-1) + a1^2*(a1+1)-2*a0*a1*(a1+2))/(a0*a1*(a0+a1+2)*(a0+a1+3)) + 3)*v^2
-  m4 <- 3*V^2*beta(a1+2,a0)/beta(a1,a0) + (mu-y)^4*m4B +6*(beta(a1+3,a0)/beta(a1,a0)*V*(y-mu)^2 - 2*beta(a1+2,a0)/beta(a1,a0)*(y-mu)^2*(1-a0/(a1+a0))*V + V*(1-a0/(a0+a1))^3*(y-mu)^2)
-  kurt <- m4/shat^2
+  ## m4B <- (6*(a0^3 - a0^2*(2*a1-1) + a1^2*(a1+1)-2*a0*a1*(a1+2))/(a0*a1*(a0+a1+2)*(a0+a1+3)) + 3)*v^2
+  ## m4 <- 3*V^2*beta(a1+2,a0)/beta(a1,a0) + (mu-y)^4*m4B +6*(beta(a1+3,a0)/beta(a1,a0)*V*(y-mu)^2 - 2*beta(a1+2,a0)/beta(a1,a0)*(y-mu)^2*(1-a0/(a1+a0))*V + V*(1-a0/(a0+a1))^3*(y-mu)^2)
+  ## kurt <- m4/shat^2
 	
   ## calculate CIs using skewed normal
   ## TODO: use apply not loop
@@ -96,7 +97,7 @@ gr<-function(y,se,X,mu,Alpha=0.95,intercept=T,eps=0.0001, normal.CI = FALSE){
   
   ## return output
   ## TODO: discuss with Tak and sync output
-  output<- list(sample.mean=y,se=se,prior.mean=prior.mean, prior.mean.hat = mu,shrinkage=Bhat,sd.shrinkage=seB,post.intv.low=skewedmat[,1],post.mean=thetahat,post.intv.upp=skewedmat[,3],post.sd=shat,model="gr",X=X.ini,beta.new=Betahat,beta.var=Betahatvar,intercept=intercept,a.new=log(Ahat),a.var=est.var[1,1], Alpha = Alpha, weight = NA, snparam = snparam)
+  output<- list(sample.mean=y,se=se,prior.mean=prior.mean, prior.mean.hat = mu,shrinkage=Bhat,sd.shrinkage=seB,post.intv.low=skewedmat[,1],post.mean=thetahat,post.intv.upp=skewedmat[,3],post.sd=shat,model="gr",X=X.ini,beta.new=Betahat,beta.var=Betahatvar,intercept=intercept,a.new=log(Ahat),a.var=est.var, Alpha = Alpha, weight = NA, snparam = snparam)
   return(output)
 }
 
@@ -106,19 +107,19 @@ gr.ll.muknown<-function(A,y,V,mu,type){
   return(lla)
 }
 
-##log adjusted posterior for known mu
+##log adjusted posterior for unknown mu
 gr.ll.muunknown<-function(A,y,V,X,type){
-  DVA<-diag(V+A)
   DVAi <- diag(1/(V+A))
-  BetaA <- solve(t(X)%*%DVAi%*%X)%*%t(X)%*%DVAi%*%y
-  lla<-type*log(A) - 1/2*sum(log(V+A)) - 1/2*log(det(t(X)%*%DVAi%*%X)) - 1/2*t(y-X%*%BetaA)%*%DVAi%*%(y-X%*%BetaA)
+  exp1 <- t(X)%*%DVAi%*%X
+  BetaA <- chol2inv(chol(exp1))%*%t(X)%*%DVAi%*%y
+  lla <- type*log(A)+sum(dnorm(x=y,mean=X%*%BetaA,sd=sqrt(V+A),log=TRUE)) - 1/2*log(det(exp1))
   return(lla)
 }
 
 ##this function is a slightly modified version of cp.to.dp in the sn package
 ##Author: Adelchi Azzalini
 ##Webpage: http://azzalini.stat.unipd.it/SN/
-gr.cp.to.dp <- function (param) { 
+gr.cp.to.dp <- function (param) {
   b <- sqrt(2/pi)
   m <- length(param) - 2
   gamma1 <- param[m + 2]
@@ -145,20 +146,20 @@ derval <- function(alpha,y,V,X){
   A = exp(alpha)
   wv = 1/(V+A)
   Wm = diag(wv)
-  Sigmam = solve(t(X)%*%Wm%*%X)
-  Betahat <- solve(t(X)%*%Wm%*%X)%*%t(X)%*%Wm%*%y
+  tXwm <- t(X)%*%Wm
+  Sigmam = chol2inv(chol(tXwm%*%X))
+  Betahat <- Sigmam%*%tXwm%*%y
+  res <- (y-X%*%Betahat)
+  exp1 <- Sigmam%*%t(X)%*%Wm^2
+  l2 = log(A) - 1/2*sum(log(V+A)) + 1/2*log(det(Sigmam)) - 1/2*sum(wv*res^2)
 
-  l2 = log(A) - 1/2*sum(log(V+A)) + 1/2*log(det(Sigmam)) - 1/2*sum(wv*(y-X%*%Betahat)^2)
+  dlralphaBEVAL = 1 - A/2*sum(wv) + A/2*tr(exp1%*%X) + A/2*sum(wv^2*res^2)
+  ##dlralphaBEVAL2 = -A/2*sum(wv^2*V) + A/2*tr(Sigmam%*%t(X)%*%Wm^2%*%X) + A/2*sum(wv^2*(y-X%*%Betahat)^2) + A^2/2*tr((Sigmam%*%t(X)%*%Wm^2%*%X)%*%(Sigmam%*%t(X)%*%Wm^2%*%X)) - A^2*tr(Sigmam%*%t(X)%*%Wm^3%*%X) - A^2*sum(wv^3*(y-X%*%Betahat)^2)
 
-  dlralphaBEVAL = 1 - A/2*sum(wv) + A/2*tr(Sigmam%*%t(X)%*%Wm^2%*%X) + A/2*sum(wv^2*(y-X%*%Betahat)^2)
-  dlralphaBEVAL2 = -A/2*sum(wv^2*V) + A/2*tr(Sigmam%*%t(X)%*%Wm^2%*%X) + A/2*sum(wv^2*(y-X%*%Betahat)^2) +
-    A^2/2*tr((Sigmam%*%t(X)%*%Wm^2%*%X)%*%(Sigmam%*%t(X)%*%Wm^2%*%X)) - A^2*tr(Sigmam%*%t(X)%*%Wm^3%*%X) - A^2*sum(wv^3*(y-X%*%Betahat)^2)
-
-  dbetahatA = Sigmam%*%t(X)%*%Wm^2%*%(X%*%Betahat - y)
-  dbetahatA2 = 2*Sigmam%*%t(X)%*%Wm^2%*%X%*%dbetahatA - 2*Sigmam%*%t(X)%*%Wm^3%*%(X%*%Betahat-y)
-  dl2alpha = dlralphaBEVAL + A*sum(wv*(y-X%*%Betahat)*X%*%dbetahatA)
-  dl2alpha2 = dlralphaBEVAL2 + A*sum(wv^2*V*(y-X%*%Betahat)*X%*%dbetahatA) - A^2*sum(wv*(X%*%dbetahatA)^2) +
-    A^2*sum(wv*(y-X%*%Betahat)*X%*%dbetahatA2) - A^2*sum(wv^2*(y-X%*%Betahat)*X%*%dbetahatA)
+  dbetahatA = exp1%*%res
+  ## dbetahatA2 = 2*Sigmam%*%t(X)%*%Wm^2%*%X%*%dbetahatA - 2*Sigmam%*%t(X)%*%Wm^3%*%(X%*%Betahat-y)
+  dl2alpha = dlralphaBEVAL + A*sum(wv*res*X%*%dbetahatA)
+  ##dl2alpha2 = dlralphaBEVAL2 + A*sum(wv^2*V*(y-X%*%Betahat)*X%*%dbetahatA) - A^2*sum(wv*(X%*%dbetahatA)^2) + A^2*sum(wv*(y-X%*%Betahat)*X%*%dbetahatA2) - A^2*sum(wv^2*(y-X%*%Betahat)*X%*%dbetahatA)
 
   ## return(list(alpha=alpha,l2=l2,dl2alpha=dl2alpha,dl2alpha2=dl2alpha2))
   return(dl2alpha=dl2alpha)
